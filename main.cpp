923b00b4ae2a0457a8b5ec62694315d624353e76
Scott Culp <scot.r.culp@hotmail.com>
2019-02-16 14:40:38 -0600

    Updates with steps

--- main.cpp
+++ main.cpp
@@ -2,6 +2,42 @@
 #include <string>
 using namespace std;
 
+double getHatSize(double hght, double wght)
+{
+	double sizeOfHat,
+	sizeOfHat = (wght/hght) * 2.9;
+	return sizeOfHat
+}
+double getJacketSize (double hght, double wght, int age)
+{
+	double sizeOfJacket;
+	int newAge;
+	int tens;
+	sizeOfJacket = hght * wght/288;
+	if (age > 30)
+{
+newAge = age-30;
+tens = newAge / 10;
+sizeOfJacket = sizeOfJacket + (tens * (1.0 / 8.0 ));
+} 
+return sizeOfJacket;
+}
+double getWaiastSize( double hght, double wght, int age)
+{
+	double sizeOfWaiast;
+	int newAge;
+	int twos;
+	sizeOfWaiast = wght / 5.7;
+	if (age > 28)
+	{
+		newAge = age - 28;
+		twos = newAge / 2;
+		
+		sizeOfWaiast = sizeOfWaiast + (twos * (1.0 / 10.0))
+	}
+	return sizeOfWaiast
+}
+
 double getHatSize(double, double);
 double getJacketSize(double, double, int);
 double getWaiastSize(double, double, int);
